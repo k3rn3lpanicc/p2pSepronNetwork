9a9e279acbf4766ce716afe109583129da871fe1
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2022-08-05 10:43:56 +0430

    Add files via upload

--- other apps/client.py
+++ other apps/client.py
@@ -8,7 +8,16 @@ port = 8005
  
 # connect to the server on local computer
 s.connect(('127.0.0.1', port))
+
+#s.connect(('185.110.189.18', port))
+
 #my_bytes = bytearray()
 #my_bytes.append(123)
 #my_bytes.append(125)
-s.send(b"hi")
+message = "{'command': 'sendMessage','pPacketCommand':0,'pPacketPayLoad':'salam'}"
+print (str(len(message)))
+s.send(message.encode())
+while(True):
+    message = s.recv(1024).decode()
+    print(message)
+    
