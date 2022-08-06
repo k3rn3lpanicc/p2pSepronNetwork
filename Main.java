45423c90c30879ca1ad1f87afde89e95048d383a
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2022-08-06 12:54:49 +0430

    Add files via upload

--- Main.java
+++ Main.java
@@ -15,8 +15,8 @@ public class Main {
             appCommunicator = new AppCommunicator();
             new Thread(appCommunicator).start();
             ServerSocket serverSocket = new ServerSocket(6009);
-          //Sender.sendConnect("185.110.189.18");
-          //Sender.sendMessageToAll(new Ppacket(0,"hi"));
+          Sender.sendConnect("185.110.189.18");
+        //  Sender.sendMessageToAll(new Ppacket(0,"hi"));
             while (true) {
                 Socket client;
                 client = serverSocket.accept();
